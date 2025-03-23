9a85a0f22b611475dfe92b402c40640a9e448904
JakubJKurasz <69475495+JakubKurasz@users.noreply.github.com>
2025-03-23 16:43:40 +0000

    add files

---
+++ Model.py
@@ -0,0 +1,253 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import PreProcessing
+from math import log2
+
+
+factors = [1,1, 1, 1 / 2, 1 / 4, 1 / 8, 1 / 16,1/32]
+sizes = [[1.5,2],[1,4.25],[2,4],[25/12,546/136],[2,1],[2,1],[199/100,1]]
+
+
+def gradient_penalty(critic, real, fake, alpha, train_step, device="cpu"):
+    BATCH_SIZE, C, H, W = real.shape
+    beta = torch.rand((BATCH_SIZE, 1, 1, 1)).repeat(1, C, H, W).to(device)
+    interpolated_images = real * beta + fake.detach() * (1 - beta)
+    interpolated_images.requires_grad_(True)
+
+    # Calculate critic scores
+    mixed_scores = critic(interpolated_images, alpha, train_step)
+
+    # Take the gradient of the scores with respect to the images
+    gradient = torch.autograd.grad(
+        inputs=interpolated_images,
+        outputs=mixed_scores,
+        grad_outputs=torch.ones_like(mixed_scores),
+        create_graph=True,
+        retain_graph=True,
+    )[0]
+    gradient = gradient.view(gradient.shape[0], -1)
+    gradient_norm = gradient.norm(2, dim=1)
+    gradient_penalty = torch.mean((gradient_norm - 1) ** 2)
+    return gradient_penalty
+
+#As can be seen below, the heights and widths used in the ProGAN
+#below do not both increase at a similar rate but instead the
+#width reaches its maximum width of 546 at the fourth layer
+#This decision was taken as testing revealed that when the real
+#images are downsized horizontally the general structure becomes
+# heavily distorted, therefore the unorthodox architecture below
+# was chosen to mitigate this issue
+
+#Actually upon further inspection of the real images the percieved distortion
+#is not a distortion at all but an accurate representation of the real images
+#Therefore the network below needs to be readjusted so that both width and height
+#increase linearly
+
+class Discriminator(nn.Module):
+    def __init__(self, z_dim, in_channels, img_channels=1):
+        super(Discriminator, self).__init__()
+        self.prog_blocks, self.rgb_layers = nn.ModuleList([]), nn.ModuleList([])
+        self.leaky = nn.LeakyReLU(0.2)
+        # here we work back ways from factors because the discriminator
+        # should be mirrored from the generator. So the first prog_block and
+        # rgb layer we append will work for input size 1024x1024, then 512->256-> etc
+        for i in range(len(factors) - 1, 0, -1):
+            conv_in = int(in_channels * factors[i])
+            conv_out = int(in_channels * factors[i - 1])
+            self.prog_blocks.append(ConvBlock(conv_in, conv_out, use_pixelnorm=False))
+            self.rgb_layers.append(
+                WSConv2d(img_channels, conv_in, kernel_size=1, stride=1, padding=0)
+            )
+
+        # perhaps confusing name "initial_rgb" this is just the RGB layer for 4x4 input size
+        # did this to "mirror" the generator initial_rgb
+        self.initial_rgb = WSConv2d(
+            img_channels, in_channels, kernel_size=1, stride=1, padding=0
+        )
+        self.rgb_layers.append(self.initial_rgb)
+
+        self.avg_pool = [nn.AvgPool2d(#273 * 100 546 * 100
+            kernel_size=[3,1], stride=[2,1], padding = [1,0]
+        ),nn.AvgPool2d( #137 * 50 546 * 50
+            kernel_size=[3,1], stride=[2,1], padding = [1,0]
+        ),nn.AvgPool2d( #68 * 25 546 * 25
+            kernel_size=[3,1], stride=[2,1], padding = [1,0]
+        ),nn.AvgPool2d( #34 * 12 136 * 12
+            kernel_size=[3,3], stride=[2,4], padding = 0
+        ),nn.AvgPool2d( #17 * 6 34 * 6
+            kernel_size=[3,3], stride=[2,4], padding = [1,0]
+        ),nn.AvgPool2d( #8 * 6
+            kernel_size=[1,3], stride=[1,4], padding = 0
+        ),nn.AvgPool2d( #4 * 4
+            kernel_size=[5,3], stride=[1,2], padding = [1,1]
+        ) ] # down sampling using avg pool
+        # this is the block for 4x4 input size
+        self.final_block = nn.Sequential(
+            # +1 to in_channels because we concatenate from MiniBatch std
+            WSConv2d(in_channels + 1, in_channels, kernel_size=3, padding=1),
+            nn.LeakyReLU(0.2),
+            WSConv2d(in_channels, in_channels, kernel_size=4, padding=0, stride=1),
+            nn.LeakyReLU(0.2),
+            WSConv2d(
+                in_channels, 1, kernel_size=1, padding=0, stride=1
+            ),  # we use this instead of linear layer
+        )
+
+    def fade_in(self, alpha, downscaled, out):
+        """Used to fade in downscaled using avg pooling and output from CNN"""
+        # alpha should be scalar within [0, 1], and upscale.shape == generated.shape
+        return alpha * out + (1 - alpha) * downscaled
+
+    def minibatch_std(self, x):
+        batch_statistics = (
+            torch.std(x, dim=0).mean().repeat(x.shape[0], 1, x.shape[2], x.shape[3])
+        )
+        # we take the std for each example (across all channels, and pixels) then we repeat it
+        # for a single channel and concatenate it with the image. In this way the discriminator
+        # will get information about the variation in the batch/image
+        return torch.cat([x, batch_statistics], dim=1)
+
+    def forward(self, x, alpha, steps):
+        # where we should start in the list of prog_blocks, maybe a bit confusing but
+        # the last is for the 4x4. So example let's say steps=1, then we should start
+        # at the second to last because input_size will be 8x8. If steps==0 we just
+        # use the final block
+        cur_step = len(self.prog_blocks) - steps
+
+        # convert from rgb as initial step, this will depend on
+        # the image size (each will have it's on rgb layer)
+        out = self.leaky(self.rgb_layers[cur_step](x))
+
+        if steps == 0:  # i.e, image is 4x4
+            out = self.minibatch_std(out)
+            return self.final_block(out).view(out.shape[0], -1)
+
+        # because prog_blocks might change the channels, for down scale we use rgb_layer
+        # from previous/smaller size which in our case correlates to +1 in the indexing
+        length = len(self.avg_pool)
+        avgPool = self.avg_pool[length - steps]
+        downscaled = self.leaky(self.rgb_layers[cur_step + 1](avgPool(x)))
+
+        out = avgPool(self.prog_blocks[cur_step](out))
+
+        # the fade_in is done first between the downscaled and the input
+        # this is opposite from the generator
+        out = self.fade_in(alpha, downscaled, out)
+
+        for step in range(cur_step+1, len(self.prog_blocks)):
+            out = self.prog_blocks[step](out)
+            length = len(self.avg_pool)
+            avgPool = self.avg_pool[step]
+            out = avgPool(out)
+
+        out = self.minibatch_std(out)
+        return self.final_block(out).view(out.shape[0], -1)
+
+
+class Generator(nn.Module):
+    def __init__(self, z_dim, in_channels, img_channels=1):
+        super(Generator, self).__init__()
+
+        # initial takes 1x1 -> 4x4
+        self.initial = nn.Sequential(
+            PixelNorm(),
+            nn.ConvTranspose2d(z_dim, in_channels, 4, 1, 0),
+            nn.LeakyReLU(0.2),
+            WSConv2d(in_channels, in_channels, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(0.2),
+            PixelNorm(),
+        )
+
+        self.initial_rgb = WSConv2d(
+            in_channels, img_channels, kernel_size=1, stride=1, padding=0
+        )
+        self.prog_blocks, self.rgb_layers = (
+            nn.ModuleList([]),
+            nn.ModuleList([self.initial_rgb]),
+        )
+
+
+        for i in range(
+            len(factors) - 1
+        ):  # -1 to prevent index error because of factors[i+1]
+            conv_in_c = int(in_channels * factors[i])
+            conv_out_c = int(in_channels * factors[i + 1])
+            self.prog_blocks.append(ConvBlock(conv_in_c, conv_out_c))
+            self.rgb_layers.append(
+                WSConv2d(conv_out_c, img_channels, kernel_size=1, stride=1, padding=0)
+            )
+
+
+    def fade_in(self, alpha, upscaled, generated):
+        # alpha should be scalar within [0, 1], and upscale.shape == generated.shape
+        return torch.tanh(alpha * generated + (1 - alpha) * upscaled)
+
+    def forward(self, x, alpha, steps):
+        out = self.initial(x)
+        if steps == 0:
+            return self.initial_rgb(out)
+
+        for step in range(steps):
+            upscaled = F.interpolate(out, scale_factor=sizes[step], mode="nearest")
+            out = self.prog_blocks[step](upscaled)
+
+        # The number of channels in upscale will stay the same, while
+        # out which has moved through prog_blocks might change. To ensure
+        # we can convert both to rgb we use different rgb_layers
+        # (steps-1) and steps for upscaled, out respectively
+        final_upscaled = self.rgb_layers[steps - 1](upscaled)
+        final_out = self.rgb_layers[steps](out)
+        return self.fade_in(alpha, final_upscaled, final_out)
+
+
+        return self.fade_in(alpha, upscaled, out)
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, use_pixelnorm=True):
+        super(ConvBlock, self).__init__()
+        self.use_pn = use_pixelnorm
+        self.conv1 = WSConv2d(in_channels, out_channels)
+        self.conv2 = WSConv2d(out_channels, out_channels)
+        self.leaky = nn.LeakyReLU(0.2)
+        self.pn = PixelNorm()
+
+    def forward(self, x):
+        x = self.leaky(self.conv1(x))
+        x = self.pn(x) if self.use_pn else x
+        x = self.leaky(self.conv2(x))
+        x = self.pn(x) if self.use_pn else x
+        return x
+    
+class WSConv2d(nn.Module):
+    """
+    Weight scaled Conv2d (Equalized Learning Rate)
+    Note that input is multiplied rather than changing weights
+    this will have the same result.
+
+    Inspired and looked at:
+    https://github.com/nvnbny/progressive_growing_of_gans/blob/master/modelUtils.py
+    """
+
+    def __init__(
+        self, in_channels, out_channels, kernel_size=3, stride=1, padding=1, gain=2
+    ):
+        super(WSConv2d, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)
+        self.scale = (gain / (in_channels * (kernel_size ** 2))) ** 0.5
+        self.bias = self.conv.bias
+        self.conv.bias = None
+
+        # initialize conv layer
+        nn.init.normal_(self.conv.weight)
+        nn.init.zeros_(self.bias)
+
+    def forward(self, x):
+        return self.conv(x * self.scale) + self.bias.view(1, self.bias.shape[0], 1, 1)
+
+class PixelNorm(nn.Module):
+    def __init__(self):
+        super(PixelNorm, self).__init__()
+        self.epsilon = 1e-8
+
+    def forward(self, x):
+        return x / torch.sqrt(torch.mean(x ** 2, dim=1, keepdim=True) + self.epsilon)
---
+++ Run.py
@@ -0,0 +1,143 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from math import log2
+from PreProcessing import preProcessing
+from Model import Generator
+from Model import Discriminator
+from Model import gradient_penalty
+
+# Hyperparameters etc.
+device = "cuda" if torch.cuda.is_available() else "cpu"
+LEARNING_RATE = 1e-3
+BATCH_SIZES = [32, 32, 16, 16, 16, 8, 4, 4]
+PROGRESSIVE_EPOCHS = [30,50,50,150,200,200,200,200]
+CHANNELS_IMG = 1
+Z_DIM = 100
+NUM_EPOCHS = 500
+FEATURES_CRITIC = 512
+FEATURES_GEN = 512
+CRITIC_ITERATIONS = 5
+LAMBDA_GP = 10
+START_TRAIN_AT_IMG_SIZE = 4
+
+def train_fn(
+    critic,
+    gen,
+    loader,
+    dataset,
+    step,
+    alpha,
+    opt_critic,
+    opt_gen,
+    tensorboard_step,
+    scaler_gen,
+    scaler_critic
+):
+    for batch_idx, (real, _) in enumerate(train_dl):
+        real = real.to(device)
+        cur_batch_size = real.shape[0]
+
+        # Train Critic: max E[critic(real)] - E[critic(fake)] <-> min -E[critic(real)] + E[critic(fake)]
+        # which is equivalent to minimizing the negative of the expression
+        noise = torch.randn(cur_batch_size, 100, 1, 1).to(device)
+
+        with torch.cuda.amp.autocast():
+            fake = gen(noise, alpha, step)
+            critic_real = critic(real, alpha, step)
+            critic_fake = critic(fake.detach(), alpha, step)
+            gp = gradient_penalty(critic, real, fake, alpha, step, device)
+            loss_critic = (
+                -(torch.mean(critic_real) - torch.mean(critic_fake))
+                + LAMBDA_GP * gp
+                + (0.001 * torch.mean(critic_real ** 2))
+            )
+
+        opt_critic.zero_grad()
+        scaler_critic.scale(loss_critic).backward()
+        scaler_critic.step(opt_critic)
+        scaler_critic.update()
+
+        # Train Generator: max E[critic(gen_fake)] <-> min -E[critic(gen_fake)]
+        with torch.cuda.amp.autocast():
+            gen_fake = critic(fake, alpha, step)
+            loss_gen = -torch.mean(gen_fake)
+
+        opt_gen.zero_grad()
+        scaler_gen.scale(loss_gen).backward()
+        scaler_gen.step(opt_gen)
+        scaler_gen.update()
+
+        # Update alpha and ensure less than 1
+        alpha += cur_batch_size / (
+            (PROGRESSIVE_EPOCHS[step] * 0.5) * dataset
+        )
+        alpha = min(alpha, 1)
+        if batch_idx % 44 == 0 and batch_idx > 0:
+            with torch.no_grad():
+                fake = gen(fixed_noise,alpha, step) * 0.5 + 0.5
+                # take out (up to) 32 examples
+                img_grid_fake = torchvision.utils.make_grid(fake[:32], normalize=True)
+
+
+                output_path = "fake_image.png"
+                torchvision.utils.save_image(img_grid_fake, output_path)
+            tensorboard_step += 1
+
+    return tensorboard_step, alpha
+
+Sizes2 = [[4,4],[8,6],[34,6],[136,12],[546,25],[546,50],[546,100],[546,199]]
+
+
+# initialize gen and disc, note: discriminator should be called critic,
+# according to WGAN paper (since it no longer outputs between [0, 1])
+gen = Generator(Z_DIM, FEATURES_GEN, CHANNELS_IMG).to(device)
+critic = Discriminator(Z_DIM, FEATURES_CRITIC, CHANNELS_IMG).to(device)
+
+
+# initializate optimizer
+opt_gen = optim.Adam(gen.parameters(), lr=LEARNING_RATE, betas=(0.0, 0.99))
+opt_critic = optim.Adam(critic.parameters(), lr=LEARNING_RATE, betas=(0.0, 0.99))
+
+scaler_critic = torch.cuda.amp.GradScaler()
+scaler_gen = torch.cuda.amp.GradScaler()
+
+# for tensorboard plotting
+fixed_noise = torch.randn(32, Z_DIM, 1, 1).to(device)
+
+
+step = 0
+
+gen.train()
+critic.train()
+
+tensorboard_step = 0
+    # start at step that corresponds to img size that we set in config
+step = int(log2(START_TRAIN_AT_IMG_SIZE / 4))
+for num_epochs in PROGRESSIVE_EPOCHS[step:]:
+        alpha = 1e-5  # start with very low alpha
+        print(Sizes2[step])
+        train_dl, test_dl, dataset = preProcessing(Sizes2[step])
+
+        for epoch in range(num_epochs):
+            print(f"Epoch [{epoch+1}/{num_epochs}]")
+            tensorboard_step, alpha = train_fn(
+                critic,
+                gen,
+                train_dl,
+                dataset,
+                step,
+                alpha,
+                opt_critic,
+                opt_gen,
+                tensorboard_step,
+                scaler_gen,
+                scaler_critic,
+            )
+
+        step += 1  # progress to the next img size
+
